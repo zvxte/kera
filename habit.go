31a1f1c21918f2c8a7e938f8b0dfc2110bcaafdc
zvxte <zvxte.codes@gmail.com>
2024-09-30 13:34:28 +0200

    create habit.go source file with initial data structures

---
+++ habit.go
@@ -0,0 +1,51 @@
+package main
+
+import "time"
+
+type Habit struct {
+	ID              UUID
+	Status          HabitStatus
+	Title           string
+	Description     string
+	StartDate       time.Time
+	EndDate         time.Time
+	TrackedWeekDays TrackedWeekDays
+	History         HabitHistory
+}
+
+type UUID [16]byte
+
+type HabitStatus uint8
+
+const (
+	HabitActive HabitStatus = iota
+	HabitEnded
+)
+
+type TrackedWeekDays uint8
+
+const (
+	Monday TrackedWeekDays = 1 << iota
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
+)
+
+type HabitHistory []HabitDay
+
+type HabitDay struct {
+	Status HabitDayStatus
+	Date   time.Time
+}
+
+type HabitDayStatus uint8
+
+const (
+	DayUntracked HabitDayStatus = iota
+	DayDone
+	DayMissed
+	DayPending
+)
