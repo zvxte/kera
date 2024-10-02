d76e8300e25954bc605b6bd72e6158810af84c34
zvxte <zvxte.codes@gmail.com>
2024-10-02 19:35:04 +0200

    run web server

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := NewRouter()
+	server := &http.Server{
+		Addr:    ":5000",
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
