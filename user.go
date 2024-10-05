7e18e268588956739dd38777fce7e1b05f235185
zvxte <zvxte.codes@gmail.com>
2024-10-05 19:14:42 +0200

    create user struct

---
+++ user.go
@@ -0,0 +1,13 @@
+package main
+
+import "time"
+
+type User struct {
+	ID            UUID
+	Username      string
+	UsernameLower string
+	DisplayName   string
+	Password      string
+	Location      time.Location
+	CreationDate  time.Time
+}
