f45ffcc6c4134aef53cbb22963cd232689afe7b4
zvxte <zvxte.codes@gmail.com>
2024-10-16 20:40:05 +0200

    create MakeHandlerFunc() function

---
+++ server/handler/handler.go
@@ -0,0 +1,15 @@
+package handler
+
+import "net/http"
+
+type HandlerFuncWithError func(http.ResponseWriter, *http.Request) (int, error)
+
+func MakeHandlerFunc(f HandlerFuncWithError) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		statusCode, err := f(w, r)
+		if err != nil {
+			// Encoding HandlerError into JSON will never fail
+			_ = jsonResponse(w, statusCode, newHandlerError(statusCode, err.Error()))
+		}
+	}
+}
