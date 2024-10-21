927467acc34f726a41340de678aee1ac1e24f572
zvxte <zvxte.codes@gmail.com>
2024-10-21 17:28:24 +0200

    implement CreateUser() method for SqlUserStore

--- store/user.go
+++ store/user.go
@@ -3,6 +3,8 @@ package store
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/zvxte/kera/model"
 )
@@ -20,4 +22,21 @@ func NewSqlUserStore(db *sql.DB) (*SqlUserStore, error) {
 		return nil, NilDBPointerError
 	}
 	return &SqlUserStore{db}, nil
+}
+
+func (sus SqlUserStore) CreateUser(ctx context.Context, user model.User) error {
+	query := `
+	INSERT INTO users(id, username, username_lower, display_name, hashed_password, location, creation_date)
+	VALUES ($1, $2, $3, $4, $5, $6, $7);
+	`
+	_, err := sus.db.ExecContext(
+		ctx, query,
+		user.ID, user.Username, strings.ToLower(user.Username), user.DisplayName,
+		user.HashedPassword, user.Location.String(), user.CreationDate,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return nil
 }
