7f837f577e8e5b460a1504eae84ab8e827d185be
zvxte <zvxte.codes@gmail.com>
2024-10-23 01:39:51 +0200

    refactor user tests

--- model/user_test.go
+++ model/user_test.go
@@ -3,64 +3,201 @@ package model
 import (
 	"strings"
 	"testing"
+	"time"
 )
 
-func TestValidateUsername(t *testing.T) {
-	validUsernames := []string{
-		"AbcD", "1234", "abcd", "ab_cd", "_1234", "AB__CD", "ABCDabcd12345678",
-	}
-	for _, username := range validUsernames {
-		if err := validateUsername(username); err != nil {
-			t.Errorf("username %q should be valid: %v", username, err)
-		}
+func TestNewUser(t *testing.T) {
+	tests := []struct {
+		name          string
+		username      string
+		plainPassword string
+		shouldErr     bool
+	}{
+		{
+			"Valid",
+			"username",
+			"password",
+			false,
+		},
+		{
+			"Invalid username",
+			"aaa",
+			"password",
+			true,
+		},
+		{
+			"Invalid password",
+			"username",
+			"aaa",
+			true,
+		},
 	}
 
-	invalidUsernames := []string{
-		"A", "Ab", "Abc", "123", "_abc_", "____", "ABCDabcd123456789", "abcd.", "abcdπ", "ab cd",
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := NewUser(test.username, test.plainPassword)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"NewUser(%q, %q), error=%v, shouldErr=%v",
+					test.username, test.plainPassword, err, test.shouldErr,
+				)
+			}
+		})
 	}
-	for _, username := range invalidUsernames {
-		if err := validateUsername(username); err == nil {
-			t.Errorf("username %q should not be valid: %v", username, err)
-		}
+}
+
+func TestLoadUser(t *testing.T) {
+	tests := []struct {
+		name           string
+		id             UUID
+		username       string
+		displayName    string
+		hashedPassword string
+		location       *time.Location
+		creationDate   time.Time
+		shouldErr      bool
+	}{
+		{
+			"Valid",
+			UUID{},
+			"username",
+			"display name",
+			"hashed password",
+			time.UTC,
+			time.Now().UTC(),
+			false,
+		},
+		{
+			"Invalid username",
+			UUID{},
+			"aaa",
+			"display name",
+			"hashed password",
+			time.UTC,
+			time.Now().UTC(),
+			true,
+		},
+		{
+			"Invalid display name",
+			UUID{},
+			"username",
+			"  display name  ",
+			"hashed password",
+			time.UTC,
+			time.Now().UTC(),
+			true,
+		},
+		{
+			"Invalid location",
+			UUID{},
+			"username",
+			"display name",
+			"hashed password",
+			nil,
+			time.Now().UTC(),
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := LoadUser(
+				test.id, test.username, test.displayName,
+				test.hashedPassword, test.location, test.creationDate,
+			)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"LoadUser(%q, %q, %q, %q, %q, %q), error=%v, shouldErr=%v",
+					test.id, test.username, test.displayName,
+					test.hashedPassword, test.location, test.creationDate,
+					err, test.shouldErr,
+				)
+			}
+		})
+	}
+}
+
+func TestValidateUsername(t *testing.T) {
+	tests := []struct {
+		name      string
+		username  string
+		shouldErr bool
+	}{
+		{"Valid", "username", false},
+		{"Valid", "user_name", false},
+		{"Too short", "aaa", true},
+		{"Too short", "_aaa_", true},
+		{"Too long", "aaaaAAAAaaaaAAAAa", true},
+		{"Invalid", "____", true},
+		{"Invalid", "aaaa.", true},
+		{"Invalid", "aa aa", true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validateUsername(test.username)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"validateUsername(%q), error=%v, shouldErr=%v",
+					test.username, err, test.shouldErr,
+				)
+			}
+		})
 	}
 }
 
 func TestValidateDisplayName(t *testing.T) {
-	validDisplayNames := []string{
-		"AbcD", "1234", "AB CD", "ABCDabcd12345678", "!@#$",
-	}
-	for _, displayName := range validDisplayNames {
-		if err := validateDisplayName(displayName); err != nil {
-			t.Errorf("display name %q should be valid: %v", displayName, err)
-		}
+	tests := []struct {
+		name        string
+		displayName string
+		shouldErr   bool
+	}{
+		{"Valid", "display name", false},
+		{"Valid", "display name!", false},
+		{"Valid", "display_name 💻", false},
+		{"Too short", "aaa", true},
+		{"Too short", "a a a", true},
+		{"Too short", "    ", true},
+		{"Too long", "aaaaAAAAaaaaAAAAa", true},
+		{"Invalid", "display name \t", true},
+		{"Invalid", " display name ", true},
 	}
 
-	invalidDisplayNames := []string{
-		"A", "Abc", "!@#", "ABCDabcd123456789", "πππ", "    ", "ab  c", " abcd",
-	}
-	for _, displayName := range invalidDisplayNames {
-		if err := validateDisplayName(displayName); err == nil {
-			t.Errorf("display name %q should not be valid: %v", displayName, err)
-		}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validateDisplayName(test.displayName)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"validateDisplayName(%q), error=%v, shouldErr=%v",
+					test.displayName, err, test.shouldErr,
+				)
+			}
+		})
 	}
 }
 
 func TestValidatePlainPassword(t *testing.T) {
-	validPlainPasswords := []string{
-		"12345678", strings.Repeat("1", plainPasswordMaxChars),
-	}
-	for _, plainPassword := range validPlainPasswords {
-		if err := validatePlainPassword(plainPassword); err != nil {
-			t.Errorf("plain password %q should be valid: %v", plainPassword, err)
-		}
+	tests := []struct {
+		name          string
+		plainPassword string
+		shouldErr     bool
+	}{
+		{"Valid", "password", false},
+		{"Valid", "aa aa aa", false},
+		{"Valid", "display_name 💻 \t", false},
+		{"Too short", "aaaaAAA", true},
+		{"Too long", strings.Repeat("a", plainPasswordMaxChars+1), true},
 	}
 
-	invalidPlainPasswords := []string{
-		"1", "1234", "1234567", strings.Repeat("1", plainPasswordMaxChars+1),
-	}
-	for _, plainPassword := range invalidPlainPasswords {
-		if err := validatePlainPassword(plainPassword); err == nil {
-			t.Errorf("plain password %q should not be valid: %v", plainPassword, err)
-		}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validatePlainPassword(test.plainPassword)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"validatePlainPassword(%q), error=%v, shouldErr=%v",
+					test.plainPassword, err, test.shouldErr,
+				)
+			}
+		})
 	}
 }
