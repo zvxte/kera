67039dc5300186e340877295be3112821c8a9aa1
zvxte <zvxte.codes@gmail.com>
2024-10-23 01:56:51 +0200

    refactor uuid tests

--- model/uuid_test.go
+++ model/uuid_test.go
@@ -9,23 +9,61 @@ func TestNewUUIDv7(t *testing.T) {
 }
 
 func TestParseUUID(t *testing.T) {
-	validValue := "0192a14e-0605-726d-89ed-6b39dae2ea91"
-	if _, err := ParseUUID(validValue); err != nil {
-		t.Error(err)
+	tests := []struct {
+		name      string
+		value     string
+		shouldErr bool
+	}{
+		{"Valid", "0192a14e-0605-726d-89ed-6b39dae2ea91", false},
+		{"Invalid", "0192a14e-", true},
+		{"Invalid", "", true},
 	}
 
-	invalidValue := ""
-	if _, err := ParseUUID(invalidValue); err == nil {
-		t.Error(err)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := ParseUUID(test.value)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"ParseUUID(%q), error=%v, shouldErr=%v",
+					test.value, err, test.shouldErr,
+				)
+			}
+		})
 	}
 }
 
-func TestUUID(t *testing.T) {
-	t.Run("String", func(t *testing.T) {
-		validValue := "0192a14e-0605-726d-89ed-6b39dae2ea91"
-		id, _ := ParseUUID(validValue)
-		if result := id.String(); result != validValue {
-			t.Errorf("expected UUID string %q, but got %q", validValue, result)
-		}
-	})
+func TestString(t *testing.T) {
+	tests := []struct {
+		name     string
+		id       UUID
+		shouldBe string
+	}{
+		{
+			"Valid all zeros",
+			UUID{},
+			"00000000-0000-0000-0000-000000000000",
+		},
+		{
+			"Valid random",
+			UUID{
+				0x55, 0x0e, 0x84, 0x00,
+				0xe2, 0x9b, 0x41, 0xd4,
+				0xa7, 0x16, 0x44, 0x66,
+				0x55, 0x44, 0x00, 0x00,
+			},
+			"550e8400-e29b-41d4-a716-446655440000",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := test.id.String()
+			if result != test.shouldBe {
+				t.Errorf(
+					"UUID.String(), got=%q, expected=%q",
+					result, test.shouldBe,
+				)
+			}
+		})
+	}
 }
