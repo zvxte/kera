60417e4e9517b0a3b93af316ec91383ef4ed4732
zvxte <zvxte.codes@gmail.com>
2024-10-25 21:22:03 +0200

    change sessions.id data type to BYTEA

--- database/migrations/002_create_sessions.sql
+++ database/migrations/002_create_sessions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS sessions(
-    id VARCHAR(32) NOT NULL PRIMARY KEY,
+    id BYTEA NOT NULL PRIMARY KEY,
     user_id UUID NOT NULL REFERENCES users(id),
     creation_date DATE NOT NULL,
     expiration_date DATE NOT NULL
