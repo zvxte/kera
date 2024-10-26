15143a1eb8c0d2bc7428ff07dd95616330289deb
zvxte <zvxte.codes@gmail.com>
2024-10-26 20:10:48 +0200

    add Create() method on session store interface

--- store/session.go
+++ store/session.go
@@ -1,3 +1,42 @@
 package store
 
-type SessionStore interface{}
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/zvxte/kera/model"
+)
+
+type SessionStore interface {
+	Create(ctx context.Context, session model.Session, userID model.UUID) error
+}
+
+type SqlSessionStore struct {
+	db *sql.DB
+}
+
+func NewSqlSessionStore(db *sql.DB) (*SqlSessionStore, error) {
+	if db == nil {
+		return nil, NilDBPointerError
+	}
+	return &SqlSessionStore{db}, nil
+}
+
+func (sss SqlSessionStore) Create(
+	ctx context.Context, session model.Session, userID model.UUID,
+) error {
+	query := `
+	INSERT INTO sessions(id, user_id, creation_date, expiration_date)
+	VALUES ($1, $2, $3, $4);
+	`
+	_, err := sss.db.ExecContext(
+		ctx, query,
+		session.HashedID, userID, session.CreationDate, session.ExpirationDate,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create session: %w", err)
+	}
+
+	return nil
+}
