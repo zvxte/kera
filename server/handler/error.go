532bb66070d18c5f2ac57e5a99f43e541ca8f175
zvxte <zvxte.codes@gmail.com>
2024-10-26 20:12:03 +0200

    add errors for common http status codes

--- server/handler/error.go
+++ server/handler/error.go
@@ -1,5 +1,14 @@
 package handler
 
+import "errors"
+
+var (
+	ErrInternalServer       = errors.New("internal server error")
+	ErrMethodNotAllowed     = errors.New("method not allowed")
+	ErrBadRequest           = errors.New("bad request")
+	ErrUnsupportedMediaType = errors.New("unsupported media type")
+)
+
 type handlerError struct {
 	StatusCode int    `json:"status_code"`
 	Message    string `json:"message"`
