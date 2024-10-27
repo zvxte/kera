f6d633efae6cf6752db2d8802ddba2e0258bf980
zvxte <zvxte.codes@gmail.com>
2024-10-27 17:24:01 +0100

    implement Login() endpoint

--- server/handler/auth.go
+++ server/handler/auth.go
@@ -8,12 +8,14 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/zvxte/kera/hash/argon2id"
 	"github.com/zvxte/kera/model"
 	"github.com/zvxte/kera/store"
 )
 
 var (
 	ErrUsernameAlreadyTaken = errors.New("username is already taken")
+	ErrInvalidCredentials   = errors.New("username or password is invalid")
 )
 
 type userIn struct {
@@ -50,7 +52,73 @@ func NewAuthHandler(
 }
 
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) (int, error) {
-	return 200, nil
+	if r.Method != http.MethodPost {
+		return http.StatusMethodNotAllowed, ErrMethodNotAllowed
+	}
+	if r.Header.Get("Content-Type") != "application/json" {
+		return http.StatusUnsupportedMediaType, ErrUnsupportedMediaType
+	}
+
+	var in userIn
+	if err := json.NewDecoder(r.Body).Decode(&in); err != nil {
+		return http.StatusBadRequest, ErrBadRequest
+	}
+
+	if err := model.ValidateUsername(in.Username); err != nil {
+		return http.StatusUnprocessableEntity, ErrInvalidCredentials
+	}
+
+	if err := model.ValidatePlainPassword(in.PlainPassword); err != nil {
+		return http.StatusUnprocessableEntity, ErrInvalidCredentials
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	user, err := h.userStore.GetByUsername(ctx, in.Username)
+	if err != nil {
+		h.logger.Println(err)
+		return http.StatusInternalServerError, ErrInternalServer
+	}
+
+	if user == nil {
+		return http.StatusUnprocessableEntity, ErrInvalidCredentials
+	}
+
+	isValid, err := argon2id.VerifyHash(in.PlainPassword, user.HashedPassword)
+	if err != nil {
+		h.logger.Println(err)
+		return http.StatusInternalServerError, ErrInternalServer
+	}
+
+	if !isValid {
+		return http.StatusUnprocessableEntity, ErrInvalidCredentials
+	}
+
+	sessionID, err := model.NewSessionID()
+	if err != nil {
+		return http.StatusInternalServerError, ErrInternalServer
+	}
+
+	session := model.NewSession(sessionID)
+
+	err = h.sessionStore.Create(ctx, session, user.ID)
+	if err != nil {
+		h.logger.Println(err)
+		return http.StatusInternalServerError, ErrInternalServer
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_id",
+		Value:    sessionID,
+		Path:     "/",
+		Secure:   true,
+		HttpOnly: true,
+		Expires:  session.ExpirationDate,
+		SameSite: http.SameSiteStrictMode,
+	})
+
+	return http.StatusOK, nil
 }
 
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) (int, error) {
