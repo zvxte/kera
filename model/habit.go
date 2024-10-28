1a4629bc92abac01d278c3e133c2c7e8fb1ed972
zvxte <zvxte.codes@gmail.com>
2024-10-28 16:25:04 +0100

    create NewHabit(), newTrackedWeekDays() and validation functions, create tests

--- model/habit.go
+++ model/habit.go
@@ -1,6 +1,36 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+	"unicode"
+	"unicode/utf8"
+)
+
+const (
+	titleMinChars = 2
+	titleMaxChars = 64
+
+	descriptionMaxChars = 256
+
+	trackedWeekDaysMin = 1
+	trackedWeekDaysMax = (1 << 7) - 1
+)
+
+var (
+	errTitleTooShort          = errors.New("title is too short")
+	errTitleTooLong           = errors.New("title is too long")
+	errTitleInvalid           = errors.New("title is invalid")
+	errDescriptionTooLong     = errors.New("description is too long")
+	errDescriptionInvalid     = errors.New("description is invalid")
+	errTrackedWeekDaysInvalid = errors.New(
+		"tracked days of the week are invalid: unrecognized day specified",
+	)
+	errTrackedWeekDaysEmpty = errors.New(
+		"at least one day of the week must be specified for tracking",
+	)
+)
 
 type Habit struct {
 	ID              UUID
@@ -8,9 +38,9 @@ type Habit struct {
 	Title           string
 	Description     string
 	TrackedWeekDays TrackedWeekDays
+	History         HabitHistory
 	StartDate       time.Time
 	EndDate         time.Time
-	History         HabitHistory
 }
 
 type HabitStatus uint8
@@ -22,8 +52,10 @@ const (
 
 type TrackedWeekDays uint8
 
+type WeekDay uint8
+
 const (
-	Monday TrackedWeekDays = 1 << iota
+	Monday WeekDay = 1 << iota
 	Tuesday
 	Wednesday
 	Thursday
@@ -47,3 +79,120 @@ const (
 	DayMissed
 	DayPending
 )
+
+func NewHabit(
+	title, description string,
+	weekDays ...WeekDay,
+) (*Habit, error) {
+	if err := validateTitle(title); err != nil {
+		return nil, err
+	}
+	if err := validateDescription(description); err != nil {
+		return nil, err
+	}
+
+	trackedWeekDays, err := newTrackedWeekDays(weekDays...)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := NewUUIDv7()
+	if err != nil {
+		return nil, errInternalServer
+	}
+
+	status := HabitActive
+
+	startDate := dateNow()
+
+	endDate := time.Time{}
+
+	history := HabitHistory{}
+
+	return &Habit{
+		ID:              id,
+		Status:          status,
+		Title:           title,
+		Description:     description,
+		TrackedWeekDays: trackedWeekDays,
+		History:         history,
+		StartDate:       startDate,
+		EndDate:         endDate,
+	}, nil
+}
+
+func newTrackedWeekDays(weekDays ...WeekDay) (TrackedWeekDays, error) {
+	var trackedWeekDays TrackedWeekDays
+	for _, day := range weekDays {
+		trackedWeekDays |= TrackedWeekDays(day)
+	}
+
+	err := validateTrackedWeekDays(trackedWeekDays)
+	if err != nil {
+		return 0, err
+	}
+
+	return trackedWeekDays, nil
+}
+
+func validateTitle(title string) error {
+	// Prevents from counting runes on a large string
+	if len(title) > titleMaxChars*4 {
+		return errTitleTooLong
+	}
+
+	length := utf8.RuneCountInString(title)
+	if length < titleMinChars {
+		return errTitleTooShort
+	}
+	if length > titleMaxChars {
+		return errTitleTooLong
+	}
+
+	for _, c := range title {
+		if unicode.IsControl(c) || (unicode.IsSpace(c) && c != ' ') {
+			return errTitleInvalid
+		}
+	}
+
+	if utf8.RuneCountInString(strings.ReplaceAll(title, " ", "")) < titleMinChars {
+		return errTitleTooShort
+	}
+
+	if strings.HasPrefix(title, " ") || strings.HasSuffix(title, " ") {
+		return errTitleInvalid
+	}
+
+	return nil
+}
+
+func validateDescription(description string) error {
+	// Prevents from counting runes on a large string
+	if len(description) > descriptionMaxChars*4 {
+		return errDescriptionTooLong
+	}
+
+	length := utf8.RuneCountInString(description)
+	if length > descriptionMaxChars {
+		return errDescriptionTooLong
+	}
+
+	for _, c := range description {
+		if unicode.IsControl(c) || (unicode.IsSpace(c) && c != ' ') {
+			return errDescriptionInvalid
+		}
+	}
+
+	return nil
+}
+
+func validateTrackedWeekDays(trackedWeekDays TrackedWeekDays) error {
+	if trackedWeekDays < trackedWeekDaysMin {
+		return errTrackedWeekDaysEmpty
+	}
+	if trackedWeekDays > trackedWeekDaysMax {
+		return errTrackedWeekDaysInvalid
+	}
+
+	return nil
+}
---
+++ model/habit_test.go
@@ -0,0 +1,239 @@
+package model
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestNewHabit(t *testing.T) {
+	tests := []struct {
+		name        string
+		title       string
+		description string
+		weekDays    []WeekDay
+		shouldErr   bool
+	}{
+		{
+			"Valid",
+			"Title",
+			"Description",
+			[]WeekDay{Monday, Tuesday, Sunday},
+			false,
+		},
+		{
+			"Valid empty description",
+			"Title",
+			"",
+			[]WeekDay{Friday},
+			false,
+		},
+		{
+			"Valid utf-8",
+			"Title 😀",
+			"My description 😓",
+			[]WeekDay{Wednesday},
+			false,
+		},
+		{
+			"Invalid title",
+			"Title\n",
+			"",
+			[]WeekDay{Sunday},
+			true,
+		},
+		{
+			"Invalid description",
+			"Title",
+			"\r\n",
+			[]WeekDay{Saturday},
+			true,
+		},
+		{
+			"Invalid empty week days",
+			"Title",
+			"Description",
+			[]WeekDay{},
+			true,
+		},
+		{
+			"Invalid week days",
+			"Title",
+			"Description",
+			[]WeekDay{WeekDay(1 << 7)},
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := NewHabit(test.title, test.description, test.weekDays...)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"NewHabit(%q, %q, %v), error=%v, shouldErr=%v",
+					test.title, test.description, test.weekDays, err, test.shouldErr,
+				)
+			}
+		})
+	}
+}
+
+func TestNewTrackedWeekDays(t *testing.T) {
+	tests := []struct {
+		name      string
+		weekDays  []WeekDay
+		shouldBe  TrackedWeekDays
+		shouldErr bool
+	}{
+		{
+			"Valid",
+			[]WeekDay{Monday, Sunday},
+			TrackedWeekDays(Monday | Sunday),
+			false,
+		},
+		{
+			"Valid",
+			[]WeekDay{Tuesday},
+			TrackedWeekDays(Tuesday),
+			false,
+		},
+		{
+			"Invalid empty",
+			[]WeekDay{},
+			TrackedWeekDays(0),
+			true,
+		},
+		{
+			"Invalid day",
+			[]WeekDay{WeekDay(1 << 7)},
+			TrackedWeekDays(0),
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			trackedWeekDays, err := newTrackedWeekDays(test.weekDays...)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"newTrackedWeekDays(%v), error=%v, shouldErr=%v",
+					test.weekDays, err, test.shouldErr,
+				)
+			}
+			if trackedWeekDays != test.shouldBe {
+				t.Errorf(
+					"newTrackedWeekDays(%v), got=%v, expected=%v",
+					test.weekDays, trackedWeekDays, test.shouldBe,
+				)
+			}
+		})
+	}
+}
+
+func TestValidateTitle(t *testing.T) {
+	tests := []struct {
+		name      string
+		title     string
+		shouldErr bool
+	}{
+		{"Valid", "Title", false},
+		{"Valid", "My title", false},
+		{"Valid short", "aa", false},
+		{"Valid long", strings.Repeat("a", titleMaxChars), false},
+		{"Valid utf-8", "Title 💻π", false},
+		{"Invalid", "Title \t", true},
+		{"Invalid", "Title \n", true},
+		{"Invalid short", "a", true},
+		{"Invalid long", strings.Repeat("a", titleMaxChars+1), true},
+		{"Invalid", " a ", true},
+		{"Invalid", " aa ", true},
+		{"Invalid", "   ", true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validateTitle(test.title)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"validateTitle(%q), error=%v, shouldErr=%v",
+					test.title, err, test.shouldErr,
+				)
+			}
+		})
+	}
+}
+
+func TestValidateDescription(t *testing.T) {
+	tests := []struct {
+		name        string
+		description string
+		shouldErr   bool
+	}{
+		{"Valid", "Description", false},
+		{"Valid", "My description", false},
+		{"Valid empty", "", false},
+		{"Valid short", "aa", false},
+		{"Valid long", strings.Repeat("a", descriptionMaxChars), false},
+		{"Valid utf-8", "💻π", false},
+		{"Invalid", "Description \t", true},
+		{"Invalid", "Description \n", true},
+		{"Invalid long", strings.Repeat("a", descriptionMaxChars+1), true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validateDescription(test.description)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"validateDescription(%q), error=%v, shouldErr=%v",
+					test.description, err, test.shouldErr,
+				)
+			}
+		})
+	}
+}
+
+func TestValidateTrackedWeekDays(t *testing.T) {
+	tests := []struct {
+		name            string
+		trackedWeekDays TrackedWeekDays
+		shouldErr       bool
+	}{
+		{
+			"Valid first day",
+			TrackedWeekDays(0b_00000001),
+			false,
+		},
+		{
+			"Valid last day",
+			TrackedWeekDays(0b_01000000),
+			false,
+		},
+		{
+			"Valid entire week",
+			TrackedWeekDays(0b_01111111),
+			false,
+		},
+		{
+			"Invalid day",
+			TrackedWeekDays(0b_10000000),
+			true,
+		},
+		{
+			"Invalid empty",
+			TrackedWeekDays(0b_00000000),
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validateTrackedWeekDays(test.trackedWeekDays)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"validateTrackedWeekDays(%v), error=%v, shouldErr=%v",
+					test.trackedWeekDays, err, test.shouldErr,
+				)
+			}
+		})
+	}
+}
