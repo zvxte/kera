071813b2e618923be65c08d0c739fec0e324156c
zvxte <zvxte.codes@gmail.com>
2024-10-30 14:30:30 +0100

    update TestNewSqlDatabase() test function

--- database/database_test.go
+++ database/database_test.go
@@ -8,27 +8,64 @@ import (
 )
 
 func TestNewSqlDatabase(t *testing.T) {
-	empty := ""
-	expectedErrorMessage := "expected an error, but got nil"
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if _, err := NewSqlDatabase(ctx, empty, empty); err == nil {
-		t.Error(expectedErrorMessage)
-	}
-
-	if _, err := NewSqlDatabase(ctx, PostgresDriverName, empty); err == nil {
-		t.Error(expectedErrorMessage)
-	}
-
 	dataSourceName := os.Getenv("DSN")
 	if dataSourceName == "" {
 		t.Skip("skipping: DSN is not set")
 	}
 
-	if _, err := NewSqlDatabase(ctx, PostgresDriverName, dataSourceName); err != nil {
-		t.Error(err)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	tests := []struct {
+		name           string
+		driverName     string
+		dataSourceName string
+		shouldErr      bool
+	}{
+		{
+			"Valid",
+			PostgresDriverName,
+			dataSourceName,
+			false,
+		},
+		{
+			"Invalid: empty driver name",
+			"",
+			dataSourceName,
+			true,
+		},
+		{
+			"Invalid: empty data source name",
+			PostgresDriverName,
+			"",
+			true,
+		},
+		{
+			"Invalid: driver name",
+			"sqlite",
+			dataSourceName,
+			true,
+		},
+		{
+			"Invalid: data source name",
+			PostgresDriverName,
+			"postgres://username:password@localhost:5432/database",
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := NewSqlDatabase(
+				ctx, test.driverName, test.dataSourceName,
+			)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"NewSqlDatabase(%v, %q, %q), error=%v, shouldErr=%v",
+					ctx, test.driverName, test.dataSourceName, err, test.shouldErr,
+				)
+			}
+		})
 	}
 }
 
