53078a44394ceda1525b44357a8d7e7b15e00198
zvxte <zvxte.codes@gmail.com>
2024-10-30 14:51:42 +0100

    update TestNewMigration() test function

--- database/migration_test.go
+++ database/migration_test.go
@@ -6,27 +6,63 @@ import (
 )
 
 func TestNewMigration(t *testing.T) {
-	expectedErrorMessage := "expected an error, but got nil"
-
-	validFilePath := filepath.Join("migrations", "000_create_migrations.sql")
-	if _, err := newMigration(validFilePath); err != nil {
-		t.Error(err)
+	tests := []struct {
+		name      string
+		filePath  string
+		shouldErr bool
+	}{
+		{
+			"Valid",
+			filepath.Join(migrationsAssetsDir, "000_create_migrations.sql"),
+			false,
+		},
+		{
+			"Invalid: directory",
+			filepath.Join("migr", "000_create_migrations.sql"),
+			true,
+		},
+		{
+			"Invalid: empty directory",
+			filepath.Join("", "000_create_migrations.sql"),
+			true,
+		},
+		{
+			"Invalid: file name",
+			filepath.Join(migrationsAssetsDir, "00_create_migrations.sql"),
+			true,
+		},
+		{
+			"Invalid: file name",
+			filepath.Join(migrationsAssetsDir, "000.sql"),
+			true,
+		},
+		{
+			"Invalid: empty file name",
+			filepath.Join(migrationsAssetsDir, ""),
+			true,
+		},
+		{
+			"Invalid: file extension",
+			filepath.Join(migrationsAssetsDir, "000_create_migrations.sq"),
+			true,
+		},
+		{
+			"Invalid: empty file extension",
+			filepath.Join(migrationsAssetsDir, "000_create_migrations"),
+			true,
+		},
 	}
 
-	if _, err := newMigration(""); err == nil {
-		t.Error(expectedErrorMessage)
-	}
-
-	if _, err := newMigration(filepath.Join("migrations", "00.sql")); err == nil {
-		t.Error(expectedErrorMessage)
-	}
-
-	if _, err := newMigration(filepath.Join("migrations", "000.txt")); err == nil {
-		t.Error(expectedErrorMessage)
-	}
-
-	if _, err := newMigration(filepath.Join("migrations", "000")); err == nil {
-		t.Error(expectedErrorMessage)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := newMigration(test.filePath)
+			if (err != nil) != test.shouldErr {
+				t.Errorf(
+					"newMigration(%q), error=%v, shouldErr=%v",
+					test.filePath, err, test.shouldErr,
+				)
+			}
+		})
 	}
 }
 
