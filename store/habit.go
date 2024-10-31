90e771464e89265888db670d82b43897a8545452
zvxte <zvxte.codes@gmail.com>
2024-10-31 17:57:54 +0100

    create SqlHabitStore implementation

--- store/habit.go
+++ store/habit.go
@@ -1,3 +1,35 @@
 package store
 
-type HabitStore interface{}
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/zvxte/kera/model"
+)
+
+type HistoryParams struct {
+	date time.Time
+}
+
+type HabitStore interface {
+	Create(ctx context.Context, habit *model.Habit, userID model.UUID) error
+	GetAllByUserID(ctx context.Context, userID model.UUID) ([]*model.Habit, error)
+	GetHistory(
+		ctx context.Context,
+		habitID model.UUID,
+		params HistoryParams,
+	) (*model.HabitHistory, error)
+	UpdateHistory(ctx context.Context, habitID model.UUID, date time.Time) error
+}
+
+type SqlHabitStore struct {
+	db *sql.DB
+}
+
+func NewSqlHabitStore(db *sql.DB) (*SqlHabitStore, error) {
+	if db == nil {
+		return nil, NilDBPointerError
+	}
+	return &SqlHabitStore{db}, nil
+}
