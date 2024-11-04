eb9a4bc576651861d9bb31ed5a7efeb0d0d6f532
zvxte <zvxte.codes@gmail.com>
2024-11-04 16:12:20 +0100

    add SessionIDHeaderName and UserIDContextKey constants

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -2,6 +2,11 @@ package handler
 
 import "net/http"
 
+const (
+	SessionIDHeaderName = "session_id"
+	UserIDContextKey    = "user_id"
+)
+
 type HandlerFuncWithError func(http.ResponseWriter, *http.Request) (int, error)
 
 func MakeHandlerFunc(f HandlerFuncWithError) http.HandlerFunc {
