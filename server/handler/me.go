bdba564a6333a8db408f8b8db4e8072bf522d8f2
zvxte <zvxte.codes@gmail.com>
2024-11-04 16:13:48 +0100

    implement Get() endpoint

--- server/handler/me.go
+++ server/handler/me.go
@@ -1,26 +1,68 @@
 package handler
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/zvxte/kera/model"
 	"github.com/zvxte/kera/store"
 )
 
+type userOut struct {
+	Username     string    `json:"username"`
+	DisplayName  string    `json:"display_name"`
+	Location     string    `json:"location"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
 func NewMeMux(userStore store.UserStore, logger *log.Logger) *http.ServeMux {
-	_ = NewMeHandler(userStore, logger)
+	h := NewMeHandler(userStore, logger)
 
-	meMux := http.NewServeMux()
-
-	return meMux
+	m := http.NewServeMux()
+	m.HandleFunc("GET /{$}", MakeHandlerFunc(h.Get))
+	return m
 }
 
 type MeHandler struct {
-	userStore    store.UserStore
-	sessionStore store.SessionStore
-	logger       *log.Logger
+	userStore store.UserStore
+	logger    *log.Logger
 }
 
-func NewMeHandler(userStore store.UserStore, logger *log.Logger) AuthHandler {
-	return AuthHandler{userStore: userStore, logger: logger}
+func NewMeHandler(userStore store.UserStore, logger *log.Logger) *MeHandler {
+	return &MeHandler{userStore: userStore, logger: logger}
+}
+
+func (h *MeHandler) Get(w http.ResponseWriter, r *http.Request) (int, error) {
+	userID, ok := r.Context().Value(UserIDContextKey).(model.UUID)
+	if !ok {
+		return http.StatusInternalServerError, ErrInternalServer
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	user, err := h.userStore.Get(ctx, userID)
+	if err != nil {
+		h.logger.Println(err)
+		return http.StatusInternalServerError, ErrInternalServer
+	}
+
+	if user == nil {
+		// TODO: unset the session cookie
+		return http.StatusUnauthorized, ErrUnauthorized
+	}
+
+	err = jsonResponse(w, http.StatusOK, userOut{
+		Username:     user.Username,
+		DisplayName:  user.DisplayName,
+		Location:     user.Location.String(),
+		CreationDate: user.CreationDate,
+	})
+	if err != nil {
+		return http.StatusInternalServerError, ErrInternalServer
+	}
+
+	return http.StatusOK, nil
 }
