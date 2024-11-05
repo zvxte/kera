2c7a62a59f34cb19496e62bbc5b25e333eb698c0
zvxte <zvxte.codes@gmail.com>
2024-11-05 17:05:50 +0100

    fix typo

--- model/doc.go
+++ model/doc.go
@@ -1,4 +1,4 @@
-// Package model provides core data models used across the applicaiton.
+// Package model provides core data models used across the application.
 //
 // All date-related fields within models are stored in UTC
 // with the time portion truncated. See date.go source file.
