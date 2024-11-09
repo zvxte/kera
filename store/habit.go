7927692114c3b12ccb8bb9cafd7a46eb61c488cb
zvxte <zvxte.codes@gmail.com>
2024-11-09 23:16:41 +0100

    add GetAll, UpdateTitle, UpdateDescription, End methods to HabitStore interface

--- store/habit.go
+++ store/habit.go
@@ -15,13 +15,20 @@ type HistoryParams struct {
 
 type HabitStore interface {
 	Create(ctx context.Context, habit *model.Habit, userID model.UUID) error
-	GetAllByUserID(ctx context.Context, userID model.UUID) ([]*model.Habit, error)
-	GetHistory(
-		ctx context.Context,
-		habitID model.UUID,
-		params HistoryParams,
-	) (*model.HabitHistory, error)
-	UpdateHistory(ctx context.Context, habitID model.UUID, date time.Time) error
+	GetAll(ctx context.Context, userID model.UUID) ([]*model.Habit, error)
+	UpdateTitle(
+		ctx context.Context, id model.UUID, title string, userID model.UUID,
+	) error
+	UpdateDescription(
+		ctx context.Context, id model.UUID, description string, userID model.UUID,
+	) error
+	End(ctx context.Context, id model.UUID, userID model.UUID) error
+	// GetHistory(
+	// 	ctx context.Context,
+	// 	habitID model.UUID,
+	// 	params HistoryParams,
+	// ) (*model.HabitHistory, error)
+	// UpdateHistory(ctx context.Context, habitID model.UUID, date time.Time) error
 }
 
 type SqlHabitStore struct {
@@ -50,6 +57,115 @@ func (s SqlHabitStore) Create(
 	)
 	if err != nil {
 		return fmt.Errorf("failed to create habit: %w", err)
+	}
+
+	return nil
+}
+
+func (s SqlHabitStore) GetAll(
+	ctx context.Context, userID model.UUID,
+) ([]*model.Habit, error) {
+	query := `
+	SELECT id, status, title, description,
+		   tracked_week_days, start_date, end_date
+	FROM habits
+	WHERE user_id = $1;
+	`
+	rows, err := s.db.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all habits: %w", err)
+	}
+	defer rows.Close()
+
+	var rawID, title, description string
+	var status model.HabitStatus
+	var trackedWeekDays model.TrackedWeekDays
+	var startDate, endDate time.Time
+
+	var habits []*model.Habit
+
+	for rows.Next() {
+		err = rows.Scan(
+			&rawID, &status, &title, &description,
+			&trackedWeekDays, &startDate, &endDate,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get all habits: %w", err)
+		}
+
+		id, err := model.ParseUUID(rawID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get all habits: %w", err)
+		}
+
+		habit, err := model.LoadHabit(
+			id, status, title, description,
+			trackedWeekDays, startDate, endDate,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get all habits: %w", err)
+		}
+
+		habits = append(habits, habit)
+	}
+
+	if rows.Err() != nil {
+		return nil, fmt.Errorf("failed to get all habits: %w", err)
+	}
+
+	return habits, nil
+}
+
+func (s SqlHabitStore) UpdateTitle(
+	ctx context.Context, id model.UUID, title string, userID model.UUID,
+) error {
+	query := `
+	UPDATE habits
+	SET title = $1
+	WHERE id = $2 AND user_id = $3;
+	`
+	_, err := s.db.ExecContext(
+		ctx, query,
+		title, id, userID,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to update title: %w", err)
+	}
+
+	return nil
+}
+
+func (s SqlHabitStore) UpdateDescription(
+	ctx context.Context, id model.UUID, description string, userID model.UUID,
+) error {
+	query := `
+	UPDATE habits
+	SET description = $1
+	WHERE id = $2 AND user_id = $3;
+	`
+	_, err := s.db.ExecContext(
+		ctx, query,
+		description, id, userID,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to update description: %w", err)
+	}
+
+	return nil
+}
+
+func (s SqlHabitStore) End(ctx context.Context, id model.UUID, userID model.UUID) error {
+	query := `
+	UPDATE habits
+	SET status = $1, end_date = $2
+	WHERE status = $3 AND id = $4 AND user_id = $5;
+	`
+	_, err := s.db.ExecContext(
+		ctx, query,
+		model.HabitEnded, model.DateNow(), model.HabitActive, id, userID,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to end habit: %w", err)
 	}
 
 	return nil
