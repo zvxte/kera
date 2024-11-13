2950c97f784335f2f6e5cac4fb60667509bc49e4
zvxte <zvxte.codes@gmail.com>
2024-11-13 02:01:51 +0100

    add UNIQUE constraint to the username_lower column

--- database/migrations/001_create_users.sql
+++ database/migrations/001_create_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id UUID NOT NULL PRIMARY KEY,
     username VARCHAR(16) NOT NULL,
-    username_lower VARCHAR(16) NOT NULL,
+    username_lower VARCHAR(16) UNIQUE NOT NULL,
     display_name VARCHAR(16) NOT NULL,
     hashed_password TEXT NOT NULL,
     creation_date DATE NOT NULL
