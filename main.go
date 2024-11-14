398ff99e9b4f130cf7943cec9dbccb5d79a2bd46
zvxte <zvxte.codes@gmail.com>
2024-11-14 23:17:16 +0100

    use ADDRESS environment variable to run the server

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"os"
+
 	"github.com/zvxte/kera/server"
 )
 
 func main() {
+	address := os.Getenv("ADDRESS")
+	if address == "" {
+		panic("ADDRESS is not set")
+	}
+
 	server, err := server.NewServer()
 	if err != nil {
 		panic(err)
 	}
-	server.Run(":5000")
+	server.Run(address)
 }
